0bdeec0f34e75f54adcab6dd4d94548be91b6698
シム ウク <astralkaiser89@gmail.com>
2023-11-27 19:44:04 +0900

    Initial commit

---
+++ src/test/java/test/thymeleafpractice/ThymeleafpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package test.thymeleafpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
